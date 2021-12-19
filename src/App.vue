7aa0df757b3a8add6a1446810e567a8a5c089cbc
Felix Du <felix.du@nokia-sbell.com>
2021-12-19 20:34:24 +0800

    save

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,199 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+<div>
+  <br/>
+  <center><h2>Machine Learning Web Portal</h2></center>
+  <uploader :options="log_options" class="uploader-example">
+    <b>Upload Log Files:</b>
+    <uploader-unsupport></uploader-unsupport>
+    <uploader-drop>
+      <p>Drop files here to upload or</p>
+      <uploader-btn>select files</uploader-btn>
+      <uploader-btn :attrs="attrs">select images</uploader-btn>
+      <uploader-btn :directory="true">select folder</uploader-btn>
+    </uploader-drop>
+    <uploader-list></uploader-list>
+  </uploader>
+
+  <uploader :options="pm_options" class="uploader-example">
+    <uploader-unsupport></uploader-unsupport>
+    <uploader-drop>
+      <p>Drop files here to upload or</p>
+      <uploader-btn>select files</uploader-btn>
+      <uploader-btn :attrs="attrs">select images</uploader-btn>
+      <uploader-btn :directory="true">select folder</uploader-btn>
+    </uploader-drop>
+    <uploader-list></uploader-list>
+  </uploader>
+
+  <uploader :options="alarm_options" class="uploader-example">
+    <uploader-unsupport></uploader-unsupport>
+    <uploader-drop>
+      <p>Drop files here to upload or</p>
+      <uploader-btn>select files</uploader-btn>
+      <uploader-btn :attrs="attrs">select images</uploader-btn>
+      <uploader-btn :directory="true">select folder</uploader-btn>
+    </uploader-drop>
+    <uploader-list></uploader-list>
+  </uploader>
+
+  <div class="myform">
+  <b-form @submit="onSubmit" @reset="onReset" v-if="show">
+    <b-form-group
+      id="input-group-1"
+      label="Email address:"
+      label-for="input-1"
+      description="We'll never share your email with anyone else."
+    >
+      <b-form-input
+        id="input-1"
+        v-model="form.email"
+        type="email"
+        placeholder="Enter email"
+        required
+      ></b-form-input>
+    </b-form-group>
+
+    <b-form-group id="input-group-2" label="Your Name:" label-for="input-2">
+      <b-form-input
+        id="input-2"
+        v-model="form.name"
+        placeholder="Enter name"
+        required
+      ></b-form-input>
+    </b-form-group>
+
+    <b-form-group id="input-group-3" label="Food:" label-for="input-3">
+      <b-form-select
+        id="input-3"
+        v-model="form.food"
+        :options="foods"
+        required
+      ></b-form-select>
+    </b-form-group>
+
+    <b-form-group id="input-group-4" v-slot="{ ariaDescribedby }">
+      <b-form-checkbox-group
+        v-model="form.checked"
+        id="checkboxes-4"
+        :aria-describedby="ariaDescribedby"
+      >
+        <b-form-checkbox value="me">Check me out</b-form-checkbox>
+        <b-form-checkbox value="that">Check that out</b-form-checkbox>
+      </b-form-checkbox-group>
+    </b-form-group>
+
+    <b-button type="submit" variant="primary">Submit</b-button>
+    <b-button type="reset" variant="danger">Reset</b-button>
+  </b-form>
+  <b-card class="mt-3" header="Form Data Result">
+    <pre class="m-0">{{ form }}</pre>
+  </b-card>
+</div>
   </div>
 </template>
 
+<script>
+export default {
+  data() {
+    return {
+      log_options: {
+        target: "http://fedora.durd.site:5000/upload_logs",
+        testChunks: false,
+        chunkSize: 1024 * 1024 * 2, // 1MB
+        simultaneousUploads: 3, // 并发上传数
+        headers: {
+          "access-token": "abcd1234",
+        },
+      },
+      alarm_options: {
+        target: "http://fedora.durd.site:5000/upload_alarm",
+        testChunks: false,
+        chunkSize: 1024 * 1024 * 2, // 1MB
+        simultaneousUploads: 3, // 并发上传数
+        headers: {
+          "access-token": "abcd1234",
+        },
+      },
+      pm_options: {
+        target: "http://fedora.durd.site:5000/upload_pms",
+        testChunks: false,
+        chunkSize: 1024 * 1024 * 2, // 1MB
+        simultaneousUploads: 3, // 并发上传数
+        headers: {
+          "access-token": "abcd1234",
+        },
+      },
+      attrs: {
+        accept: "image/*",
+      },
+      form: {
+        email: "",
+        name: "",
+        food: null,
+        checked: [],
+      },
+      foods: [
+        { text: "Select One", value: null },
+        "Carrots",
+        "Beans",
+        "Tomatoes",
+        "Corn",
+      ],
+      show: true,
+    };
+  },
+  options: {
+    inquiry: [
+      { value: "feature", text: "Feature Request" },
+      { value: "bug", text: "Bug Report" },
+      { value: "support", text: "Support" },
+    ],
+  },
+  methods: {
+    onSubmit(event) {
+      event.preventDefault();
+      alert(JSON.stringify(this.form));
+    },
+    onReset(event) {
+      event.preventDefault();
+      // Reset our form values
+      this.form.email = "";
+      this.form.name = "";
+      this.form.food = null;
+      this.form.checked = [];
+      // Trick to reset/clear native browser form validation state
+      this.show = false;
+      this.$nextTick(() => {
+        this.show = true;
+      });
+    },
+  },
+};
+</script>
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+@import "https://cdnjs.cloudflare.com/ajax/libs/bulma/0.4.4/css/bulma.min.css";
+.myform {
+  width: 880px;
+  padding: 15px;
+  margin: 20px auto 0;
+  font-size: 12px;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.4);
 }
-
-#nav {
-  padding: 30px;
+.uploader-example {
+  width: 880px;
+  padding: 15px;
+  margin: 20px auto 0;
+  font-size: 12px;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.4);
 }
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
+.uploader-example .uploader-btn {
+  margin-right: 4px;
 }
-
-#nav a.router-link-exact-active {
-  color: #42b983;
+.uploader-example .uploader-list {
+  max-height: 120px;
+  overflow: auto;
+  overflow-x: hidden;
+  overflow-y: auto;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,10 +1,30 @@
 import Vue from 'vue';
+import uploader from 'vue-simple-uploader';
 import App from './App.vue';
 import router from './router';
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+Vue.use(BootstrapVue)
+Vue.use(IconsPlugin)
+Vue.use(uploader);
 Vue.config.productionTip = false;
 
 new Vue({
   router,
   render: (h) => h(App),
 }).$mount('#app');
+
+// import Vue from 'vue'
+// import uploader from 'vue-simple-uploader'
+// import App from './App.vue'
+
+/* eslint-disable no-new */
+// new Vue({
+//   el: '#app',
+//   router,
+//   components: { App },
+//   template: '<App/>',
+// });
